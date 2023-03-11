4ed5ff7334002bbc5ad586079f96c9270de2f28b
PeterUpfold <pgithub@upfold.org.uk>
2023-03-11 18:14:36 +0000

    Add support for domain and relative path structure

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # dump-iphone-backup
 
-Dump an encrypted iPhone backup to a (very messy, structure-less) folder for analysis of any artifacts.
+Dump an encrypted iPhone backup to a folder for analysis of any artifacts, organised by domain and path
+of the source file.
 
 Uses https://github.com/jsharkey13/iphone_backup_decrypt
 
@@ -18,11 +19,17 @@ You may need to permit Terminal to have Full Disk Access on macOS for it to be p
      ./dump-iphone-backup.py -b ~/Library/Application\ Support/MobileSync/Backup/SOME-GUID/ -o ~/output/ -p some-passphrase
      # If comfortable, you can pass the passphrase on the command line
 
+     # Or, set an environment variable for the passphrase:
+     export DUMP_IPHONE_BACKUP_PASSPHRASE='some-passphrase'
+     ./dump-iphone-backup.py -b ~/Library/Application\ Support/MobileSync/Backup/SOME-GUID/ -o ~/output/ 
+
 ## Results
 
-This will give you a flat-file, very messy directory with every file that was backed up.
+This will
 
-While lacking structure, this repository of the backed up contents of the iOS device is useful for scanning against (for example) indicators of compromise of any future Pegasus-style spyware which may later become known.
+This repository of the backed up contents of the iOS device is useful for scanning against (for example) indicators of compromise of any future Pegasus-style spyware which may later become known.
 
 Regular backups and dumping of the encrypted backup using this tool may prove useful in shining the bright light of day on iOS spyware in the future. Perhaps even the existence of this script and the practice of regularly archiving forensic evidence of such spyware may present a disincentive to the deployment of such tools, as it increases the attacker's risk of detection and their methods.
 
+If possible, regularly back up a device and archive each extracted backup for any future scanning for
+indicators of compromise that later become known.
--- dump-iphone-backup.py
+++ dump-iphone-backup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 #
 # A simple command line client to dump an iPhone encrypted backup
 # to a flat file folder of items.
@@ -10,6 +11,8 @@ from iphone_backup_decrypt import EncryptedBackup, RelativePath, RelativePathsLi
 import argparse
 import getpass
 import os
+import sqlite3
+from contextlib import closing
 
 parser = argparse.ArgumentParser(description='Command line client to dump iPhone encrypted backup to flat folder of items.')
 parser.add_argument('-p', '--passphrase', type=str, default=None, required=False, help='The passphrase of the backup. If not passed, the tool will prompt interactively for the passphrase')
@@ -19,7 +22,10 @@ parser.add_argument('-o', '--output-path', type=str, default=None, required=True
 args = parser.parse_args()
 
 if args.passphrase is None:
-    passphrase = getpass.getpass(prompt='Provide the backup passphrase: ')
+    if os.environ.get('DUMP_IPHONE_BACKUP_PASSPHRASE') is None:
+        passphrase = getpass.getpass(prompt='Provide the backup passphrase: ')
+    else:
+        passphrase = os.environ.get('DUMP_IPHONE_BACKUP_PASSPHRASE')
 else:
     passphrase = args.passphrase
 
@@ -32,4 +38,27 @@ if not os.path.isdir(args.output_path):
 
 backup=EncryptedBackup(backup_directory=args.backup_path, passphrase=passphrase)
 
-backup.extract_files(relative_paths_like='%', output_folder=args.output_path)
+# decrypt Manifest so we can search it for domains soon
+manifest_path = os.path.join(args.output_path, 'Manifest.db')
+backup.save_manifest_file(output_filename=manifest_path)
+
+# get a list of domains and create top level folders
+with closing(sqlite3.connect(manifest_path)) as connection:
+    cursor = connection.cursor()
+    for domain_row in cursor.execute('SELECT DISTINCT domain FROM Files'):
+        os.makedirs(os.path.join(args.output_path, domain_row[0]), exist_ok=True)
+
+    # for each file in the Manifest with flags = 1, extract it
+    for file_row in cursor.execute('SELECT domain, relativePath, flags FROM Files WHERE flags = 1'):
+        domain = file_row[0]
+        relativePath = file_row[1]
+        flags = file_row[2]
+
+        # cut the last path item from relativePath
+        relative_path_without_file = os.path.split(relativePath)[:-1]
+
+        containing_dir = os.path.join(args.output_path, domain, relative_path_without_file[0])
+        os.makedirs(containing_dir, exist_ok=True)
+
+        print(f'Extracting {relativePath} from domain {domain}')
+        backup.extract_file(relative_path = relativePath, output_filename=os.path.join(args.output_path, domain, relativePath))
